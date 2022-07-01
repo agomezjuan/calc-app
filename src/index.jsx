71bb5d8b19d2b9da55b57ee1a43051a1b4feedf7
Alberto Gomez Juan <agomezjuan@hotmail.com>
2022-07-01 12:47:51 +0200

    Utilizacion de la extension JSX en vez de JS

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// Importar componente
+import App from './App';
+// Utilizar el componente
+ReactDOM.render(<App/>, document.getElementById('root'))
