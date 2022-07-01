ec7c2df23c64aee107aaf89e92036ab65ccfc73e
Alberto Gomez Juan <agomezjuan@hotmail.com>
2022-07-01 12:47:09 +0200

    Primer componente creado

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// Importaciones
+import React from 'react'
+
+// Generacion de la funcion del componente
+const App = () => {
+    return <h1>Hola mundo! (app) </h1>
+}
+
+// Exportación
+export default App
