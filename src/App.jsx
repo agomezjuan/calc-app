bf9e62dd786ce1f7e3464d7c2e81743a02dcad12
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-07-31 21:23:21 +0200

    Uso de estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 // Importaciones
-import React from 'react'
+import React, { useState } from 'react';
+import Functions from './components/Functions';
+import MathOperations from './components/MathOperations';
+import Numbers from './components/Numbers';
+import Result from './components/Result';
+import './App.css';
 
 // Generacion de la funcion del componente
 const App = () => {
-    return <h1>Hola mundo! (app) </h1>
-}
+  const [stack, setStack] = useState('');
+  return (
+    <main className="react-calculator">
+      <Result value={stack} />
+      <Numbers
+        onClickNumber={(number) => {
+          console.log('Click in number', number);
+          setStack(`${stack}${number}`);
+        }}
+      />
+      <Functions
+        onContentClear={() => {
+          console.log('Content Clear');
+          setStack('');
+        }}
+        onDelete={() => {
+          console.log('Content Delete');
+          const newStack = stack.substring(0, stack.length - 1);
+          setStack(newStack)
+        }}
+      />
+      <MathOperations
+        onClickOperation={(operation) => {
+          console.log('Operation:', operation);
+          setStack(`${stack}${operation}`);
 
+        }}
+        onClickEqual={(equal) => {
+          console.log('Equal:', equal);
+          setStack(`${stack}${equal}`);
+        }}
+      />
+    </main>
+  );
+};
 // Exportación
-export default App
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Importar componente
 import App from './App';
 // Utilizar el componente
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
